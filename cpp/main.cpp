e5ac8aa2f07951ea9c0933211e6ef1a3dd817ade
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-07-14 17:20:16 +0900

    [update] main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,13 +3,14 @@ using namespace std;
 using i64 = long long;
 #define rep(i, n) for (i64 i = 0; i < (i64)(n); i++)
 #define rep2(i, a, n) for (i64 i = (a); i < (i64)(n); i++)
+#define vsort(v) sort(v.begin(), v.end())
+#define vsortr(v) sort(v.begin(), v.end(), greater<i64>)
 #define fi first
 #define se second
 #define MOD 998244353
 typedef pair<i64, i64> P;
 
 void Main() {
-
 }
 
 int main() {
